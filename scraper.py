377b7deb01d59103993bb7e7d3c8078fa707eaff
Gedes <makspl17@gmail.com>
2019-03-21 17:56:10 +0000

    added some functions

--- scraper.py
+++ scraper.py
@@ -8,6 +8,8 @@ url = 'https://www.reddit.com/r/RoastMe/'
 
 #to get reddit's trust we need an access token from their api
 handle = 'https://api.pushshift.io/reddit/search/submission/?'
+handleC = 'https://api.pushshift.io/reddit/comment/search/?'
+handleComment = 'https://api.pushshift.io/reddit/submission/comment_ids/'
 #POST data included in the URL, kinda like an attachment
 #we're asking for Client_credentials Flow for non-installed script type app
 
@@ -18,10 +20,22 @@ handle = 'https://api.pushshift.io/reddit/search/submission/?'
 def getSubmissionsUrl(subreddit='RoastMe'):
     return handle + 'subreddit='+subreddit + '&fields=id'
 
-def writeSubmissions(url,filename):
+#subreddit name, epoch last time, sort True = descending score, false = ascending score
+def getSubmissionIdsUrl(subreddit='roastme',before='1s',after='1d',sort=True):
+    sortTxt = ''
+    if sort:
+        sortTxt = 'desc'
+    else:
+        sortTxt = 'asc'
+    return handle +'&before='+before+'&after='+after+'&sort_type=score' +'&sort=desc'+'&limit=500'+'&subreddit='+subreddit + '&fields=url,id'
+#'&after='+after+'&before=' +before
+
+def requestAndWrite(url,filename):
     r = requests.get(url).json()
     with open(filename+'.json','w') as f:
         json.dump(r,f)
+requestAndWrite(getSubmissionIdsUrl(),'lol')
 
-print(getSubmissionsUrl())
-writeSubmissions(getSubmissionsUrl(),'lol')
+def getCommentsByIdUrl(pid=''):
+    return handleC +'&link_id='+pid + '&nest_level=1' + '&fields=body,score' + '&sort=score:asc' + '&limit = 500'
+requestAndWrite(getCommentsByIdUrl('atsdhf'),'lol')
