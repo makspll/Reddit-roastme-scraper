9e6c11e498d293c06114743eb5212d73c6ba5be7
Gedes <makspl17@gmail.com>
2019-03-23 22:09:46 +0000

    working product

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import praw as pr
 import json
 import face_recognition
 import numpy as np
-import re
+import re,sys,os
 url = 'https://www.reddit.com/r/RoastMe/'
 
 
@@ -19,23 +19,20 @@ handleComment = 'https://api.pushshift.io/reddit/submission/comment_ids/'
 #username = 'l_IBFD5bZOT_IA'
 #password = 'CYdIMyeTNQrzIQb1qXBdwM71rms'
 #encoded = base64.b64encode(username+':'+password)
+# Print iterations progress
+def progress(count, total, status=''):
+    bar_len = 30
+    filled_len = int(round(bar_len * count / float(total)))
 
-def getSubmissionsUrl(subreddit='RoastMe'):
-    return handle + 'subreddit='+subreddit + '&fields=id'
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
 
-#subreddit name, epoch last time, sort True = descending score, false = ascending score
-def getSubmissionIdsUrl(subreddit='roastme',before='1s',after='1d',sort=True):
-    sortTxt = ''
-    if sort:
-        sortTxt = 'desc'
-    else:
-        sortTxt = 'asc'
-    return handle +'&before='+before+'&after='+after+'&sort_type=score' +'&sort=desc'+'&limit=500'+'&subreddit='+subreddit + '&fields=url,id'
-#'&after='+after+'&before=' +before
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
 
 def requestAndWrite(url,filename):
-    r = requests.get(url).json()
     with open(filename+'.json','w') as f:
+        r = requests.get(url).json()
         json.dump(r,f)
 
 def jsonToDict(filename):
@@ -47,103 +44,63 @@ def requestToDict(url):
     r = requests.get(url).json()
     return r
 
-def getCommentsBySubIdUrl(pid=''):
-    print(handleC +'&q=*'+'&subreddit=roastme'+'&parent_id=t1_'+pid + '&fields=body,score,parent_id')
-    return handleComment + pid
-
-def getCommentByIdUrl(id):
-    return handleC + '&ids=' + id + '&fields=body,score,id'
-
-def getCommentsByIdsUrl(ids=[]):
-    str = ''
-    for a in ids:
-            str +=  a + ','
-    str = str[0:-1]
-    return handleC + '&ids='+str + '&fields=body,score,id'
-
 #given json list of comments from subreddit get top n comments by score
 #have to have fields=[id]
-def getTopComment(database):
-    print(database)
-    list = database["data"]
-    maxId = ''
-    maxScore = 0
-    for comment in list:
-        newMaxScore = max(maxScore,requestToDict(getCommentByIdUrl(comment["id"]))["data"][0]["score"])
-        if newMaxScore != maxScore:
-            maxId = comment["id"]
-    return maxId
 
-def loadSubmissions(limit,startDay=1,subreddit='roastme'):
-    const_args = '&limit=500&fields=url,score,created_utc,id'
+def loadSubmissions(table,startDay=1,subreddit='roastme'):
+    const_args = '&limit=500&fields=url,title,score,created_utc,id&sort_type=num_comments&sort=desc'
     baseurl = handle + '&subreddit='+ subreddit + const_args
 
-    submissions_count = 0
-    curr_startDay = startDay
+    submissions = table
+    before = str(startDay) + 'd'
+    after = str(startDay+1) + 'd'
 
-    submissions = []
-    print("loading Submissions")
-    while submissions_count <= limit:
-        before = str(curr_startDay) + 'd'
-        after = str(curr_startDay+1) + 'd'
-        data_dict = requestToDict(baseurl+'&after='+after+'&before='+before)
-        capacity = limit - submissions_count
-        submissions.extend(data_dict["data"][:capacity])
-        submissions_count += len(data_dict["data"])
-        print("current count: " + str(submissions_count))
-    return submissions
+    data_dict = requestToDict(baseurl+'&after='+after+'&before='+before)
+
+    data = data_dict["data"]
+    #print(data)
+    if len(data) == 0:
+        print("NO MORE DATA, could not get requested number, found: "+str(submissions_count))
+        return -1
+    submissions.extend(data)
+        #print("current count: " + str(submissions_count))
+    return startDay
 #given a list of submission dictionaries update their score fields and remove ones
 #without comments or below upvote threshold
-def updateSubmissions(submissions,score_threshold,comments_threshold):
-    filtered = submissions
+def updateSubmissions(submissions,limit,score_threshold,comments_threshold):
+
     r = pr.Reddit(client_id='C-u-vJYPUPLdfg',
                   client_secret='Hyw3NkOJrL6qufVnIgt7EZ7Xj8k',
                   password='password123',
                   user_agent='testscript',
                   username='roastr123')
-    print("authorised reddit, filtering and updating")
-    deletable_indexes = []
-    for index,sub_dict in enumerate(filtered):
-        print("updating submission : " + str(index))
+
+    new = []
+    for index,sub_dict in enumerate(submissions):
         id = sub_dict["id"]
         praw_submission = pr.models.Submission(r,id)
-        print(str(praw_submission.score) + "," + str(praw_submission.num_comments))
+        progress(len(new),limit,'encoding')
+        if(len(new) >= limit):
+            return new
 
-        if (praw_submission.num_comments <= comments_threshold or praw_submission.score <= score_threshold \
-        or (re.match('.(jpg|png)',praw_submission.url)) != None):
-            deletable_indexes.append(index)
-            print("deleted")
-        else:
-            submissions[index]["score"] = praw_submission.score
-            submissions[index]["comments"] = loadTopComments(id,comments_threshold)
-
-            picture = requests.get(praw_submission.url)
+        if not (praw_submission.num_comments <= comments_threshold or praw_submission.score <= score_threshold \
+        or not (praw_submission.url.endswith(('.png','.jpg')))):
+            copied_submission = submissions[index].copy()
             file_name = praw_submission.url.split('/')[-1].split('.')[-1]
-            print(praw_submission.url)
-            with open('temp.'+file_name,'wb') as f:
-                f.write(picture.content)
-                submissions[index]["encodings"] = encodePicture('temp.'+file_name)
 
-    removeIndexes(deletable_indexes,filtered)
-def updateComments(comments,score_threshold):
-    filtered = comments
-    r = pr.Reddit(client_id='C-u-vJYPUPLdfg',
-                  client_secret='Hyw3NkOJrL6qufVnIgt7EZ7Xj8k',
-                  password='password123',
-                  user_agent='testscript',
-                  username='roastr123')
-    print("authorised reddit, filtering and updating")
-    deletable_indexes = []
-    for index,com_dict in enumerate(filtered):
-        print("updating comment : " + str(index))
-        praw_submission = pr.models.Comment(r,com_dict["id"])
-        print(str(praw_submission.score))
-        if (praw_submission.score <= score_threshold):
-            deletable_indexes.append(index)
-            print("deleted")
-        else:
-            comments[index]["score"] = praw_submission.score
-    removeIndexes(deletable_indexes,filtered)
+            with open('temp.'+file_name,'wb') as f:
+                picture = requests.get(praw_submission.url)
+
+                f.write(picture.content)
+                encodings = encodePicture('temp.'+file_name)
+                if len(encodings) != 0:
+                    copied_submission["score"] = praw_submission.score
+                    copied_submission["comments"] = loadTopComments(id,comments_threshold)
+                    copied_submission["encodings"] = encodings
+                    new.append(copied_submission.copy())
+    #removeIndexes(deletable_indexes,filtered)
+    return new
+
 def loadTopComments(id,limit):
     r = pr.Reddit(client_id='C-u-vJYPUPLdfg',
                   client_secret='Hyw3NkOJrL6qufVnIgt7EZ7Xj8k',
@@ -162,7 +119,10 @@ def loadTopComments(id,limit):
     return comments
 
 def encodePicture(filename):
-    pic = face_recognition.api.load_image_file(filename)
+    try:
+        pic = face_recognition.api.load_image_file(filename)
+    except:
+        return []
     encodings = face_recognition.api.face_encodings(pic)
     faces = []
     for encoding in encodings:
@@ -172,40 +132,79 @@ def encodePicture(filename):
 #given a list of dictionaries of submissions merge comments and format for poem
 def getAllComments(data):
     formated_comments = []
-    for dict in data:
+    for index,dict in enumerate(data):
         comments = dict["comments"]
         for comment in comments:
             formated_comments.append(formatToPoem(comment))
     return formated_comments
 
+#----------------------------USEFUL PUBLIC STUFF ------------------------------#
 
-def writeAllFormatedComments(datafile,filename):
+#give it the datafile name of the database and will create linguistic database with filename
     saveData(getAllComments(loadData(datafile)),filename)
+    def writeAllFormatedComments(datafile,filename):
 
-def removeIndexes(indexes,array):
-    for i in sorted(indexes,reverse=True):
-        array.pop(i)
 
+#this is just for general clean up
 def formatComment(comment):
-    return comment
+    return bytes(comment.strip(),'utf-8').decode('utf-8','ignore')
 
+
+
+
+#fiona use this to format, if isn't as clean as you need, make changes here
 def formatToPoem(comment):
-    return comment
+    newlines = re.sub(r'\n',' ',comment.lower().strip('. '))
+    punctuated = re.sub(r'[!?]','.',newlines)
+    repetitions = re.sub(r'(([^A-Za-z])\2\2*)',r'\2',punctuated)
+    characterReptitions = re.sub(r'((.)\2\2\2*)',r'\2',repetitions)
+    result = re.sub('[^A-Za-z.\"\' ]','',characterReptitions)
+    return result
 
-def saveData(dic,filename):
-    with open(filename+'.json','w',encoding='utf-8') as f:
-        json.dump(dic,f)
 
+
+#filepath without extension !
+#will append existing data if same file exists
+def saveData(data,filename):
+    if not os.path.isfile(filename+'.json'):
+        with open(filename+'.json','w') as f:
+            json.dump(data,f)
+    else:
+        feeds = loadData(filename)
+
+        feeds.extend(dic)
+        with open(filename+'.json',mode = 'w') as f:
+            f.write(json.dumps(feeds))
+#will pull into list of dictionaries
 def loadData(filename):
-    with open(filename+'.json','r',encoding='utf-8') as f:
+    with open(filename+'.json','r') as f:
         return json.load(f)
 
+
+#the sub has around 236,000 submissions over 3 years
+#takes in:
+#filename for data, ling data
+#amount of submissions to pull
+#minimum upvotes (too high might end up with not many submissions)
+#minimum comments (same as comments pulled)
+#optional - days offset (integer how many days ago to start indexing)
 #all you need, right here, filenames are paths without extension
 def createDatabase(data_filename,linguistic_filename,limit=10,upvote_threshold=50,comment_threshold=3,days_offset=1):
-    raw_submissions = loadSubmissions(limit,days_offset)
-    updateSubmissions(raw_submissions,upvote_threshold,comment_threshold)
-    saveData(raw_submissions,data_filename)
-    writeAllFormatedComments(data_filename,linguistic_filename)
+    curr_startDay = days_offset
+    submissions_count = 0
 
+    while submissions_count < limit:
+        progress(submissions_count,limit,'processing')
+        submissions_batch = []
+        capacity = limit - submissions_count
 
-createDatabase('test','allTest',10,40,5)
+        loadSubmissions(submissions_batch,curr_startDay)
+        submissions_batch = updateSubmissions(submissions_batch,capacity,upvote_threshold,comment_threshold)
+
+        curr_startDay +=1
+
+        batch_count = len(submissions_batch)
+
+        saveData(submissions_batch[:capacity],data_filename)
+        submissions_count += batch_count
+        writeAllFormatedComments(data_filename,linguistic_filename)
